ff84f849cc825fb15b892a077a624131837ede15
asinai1337 <70227274+asinai1337@users.noreply.github.com>
2021-09-03 09:53:33 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+import discord
+from discord.ext import commands
+
+client = commands.Bot(command_prefix="*", intents=intents, case_sensitive=True)
+
+@client.event
+async def on_ready():
+  print("Bot online!")
+ 
+ client.run(config.TOKEN)
