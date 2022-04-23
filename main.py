102369ff917dd00f3e2e955017d29e06b7af5e93
asinai1337 <70227274+asinai1337@users.noreply.github.com>
2022-04-23 23:04:42 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import config
 
 from discord.ext import commands
 
+intents = discord.Intents().all()
+
 client = commands.Bot(command_prefix="*", intents=intents, case_sensitive=True)
 
 @client.event
